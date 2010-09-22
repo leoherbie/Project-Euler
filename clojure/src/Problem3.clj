3a108319dfe90dd35db469d7525d51fa89527899
Travis Schneeberger <leo.herbie@gmail.com>
2010-09-22 16:58:48 -0400

    start of problem3

--- clojure/src/Problem3.clj
+++ clojure/src/Problem3.clj
@@ -1,3 +1,19 @@
 ; The prime factors of 13195 are 5, 7, 13 and 29.
 ;
 ; What is the largest prime factor of the number 600851475143 ?
+
+(defn prime? [n]
+  "is a number a prime number not including 1"
+  (cond
+    (= n 1) false
+    (and (even? n) (> n 2)) false
+    (= n 2) true
+    ;call function to determine non-special cases
+  ))
+
+(defn factor? [f n]
+  "is number f is a factor of number n" (and
+   (< f n)
+   (not (ratio? (/ n f)))))
+
+(println (prime? 5))
