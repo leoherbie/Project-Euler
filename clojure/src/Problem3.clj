0a683b5fcc172e8debe7450e19e1e89bd17ffc48
Travis Schneeberger <leo.herbie@gmail.com>
2010-09-23 15:52:05 -0400

    solving problem 3

--- clojure/src/Problem3.clj
+++ clojure/src/Problem3.clj
@@ -2,10 +2,10 @@
 ;
 ; What is the largest prime factor of the number 600851475143 ?
 
-(defn factor? [f n]
-  "is number f is a factor of number n" (and
-   (< f n)
-   (not (ratio? (/ n f)))))
+(defn factor [f n]
+  "how many times does is f a factor of n. returns nil if not a factor"
+  (let [t (/ n f)] (if (ratio? t) nil t))
+  )
 
 (defn prime? [n]
   "is a number a prime number"
@@ -13,8 +13,20 @@
     (= n 1) false
     (and (even? n) (> n 2)) false
     (= n 2) true
-    true (not-any? #(factor? % n) (range 3 (- n 1)))
+    true (not-any? #(factor % n) (range 3 (- n 1)))
   ))
 
-;need to start high and work my way down...
-;(println (take 1 (filter #(and (factor? % 13195) (prime? %)) (reverse (range 2 13195)))))
+(defn largest-prime-factor [num]
+  "finds the largest prime factor of a number passed in"
+  (if (prime? num) num
+    (loop [n num cur 2 largest nil]
+      (let [f (factor cur n)]
+        (cond
+          (and f (prime? f)) f ;f must be largest
+          (and f (prime? cur)) (recur (identity n) (inc cur) (identity cur)) ;recurse w/ new prime
+          (and f (<= f cur)) largest ;stop here
+          (prime? num) num ;the number is a prime - should check this ahead of time!
+          true (recur (identity n) (inc cur) (identity largest)) ;recurse w/ no new prime
+  )))))
+
+(println (largest-prime-factor 600851475143))
