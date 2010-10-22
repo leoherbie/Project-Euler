f685bafabb9a84aca45c6ebc4a4ac15faa4f0321
Travis Schneeberger <leo.herbie@gmail.com>
2010-10-22 13:25:52 -0400

    adding solution to Problem10

---
+++ clojure/src/Problem010.clj
@@ -0,0 +1,12 @@
+; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+;
+; Find the sum of all the primes below two million.
+
+; first attempt
+(ns problems
+  (:use [common :only (prime?)]))
+
+(defn sum-of-primes-below-limit [limit]
+  (apply + (cons 2 (filter prime? (range 3 limit 2)))))
+
+(println (time (sum-of-primes-below-limit 2000000)))
